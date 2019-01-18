6f2890c44d60081a8a0fa0ae02433f639d928037
unknown <leonardo.paglionico@gmail.com>
2019-01-18 18:21:06 +0100

    Allineamento

--- scripts/sqlite/ServerJournal.sql
+++ scripts/sqlite/ServerJournal.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.2.1 on Thu Jan 17 15:28:02 2019
+-- File generated with SQLiteStudio v3.2.1 on Thu Jan 17 22:00:00 2019
 --
 -- Text encoding used: System
 --
@@ -13,7 +13,7 @@ CREATE TABLE Request (Id INTEGER PRIMARY KEY AUTOINCREMENT, OperationId TEXT NOT
 CREATE TABLE Response (Id INTEGER PRIMARY KEY AUTOINCREMENT, OperationId TEXT NOT NULL, Timestamp TEXT NOT NULL, Status INTEGER NOT NULL, RemoteIP TEXT NOT NULL, RemotePort INTEGER NOT NULL, LocalIP TEXT NOT NULL, LocalPort TEXT NOT NULL, Headers TEXT, Body TEXT);
 
 -- Table: Trace
-CREATE TABLE Trace (Scope TEXT, ServerName TEXT, User TEXT, Severity INTEGER NOT NULL, SessionId TEXT, OperationId TEXT NOT NULL, Timestamp TEXT NOT NULL, Message TEXT NOT NULL, Data TEXT);
+CREATE TABLE Trace (Id INTEGER PRIMARY KEY AUTOINCREMENT, Scope TEXT, ServerName TEXT, User TEXT, Severity INTEGER NOT NULL, SessionId TEXT, OperationId TEXT NOT NULL, Timestamp TEXT NOT NULL, Message TEXT NOT NULL, Data TEXT);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
